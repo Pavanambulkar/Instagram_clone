8392d0a8f02155ef8184426dba43dc8c22076b9e
pavan <pavanambulkar69@gmail.com>
2023-01-08 02:15:30 +0530

    front

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import  Profile from '../../Assets/Profile.jpg'
 import Button from '../../components/button';
 import Input from "../../components/input";
+import post from '../../Assets/post.jpg'
 
 const Home = () => {
   return (
@@ -43,15 +44,140 @@ const Home = () => {
 
         </div>
         <div className='w-[60%] overflow-scroll h-full scrollbar-hide '>
-            <div className='bg-white h-[75px] border-l flex justify-center items-center'>
+            <div className='bg-white h-[75px] border-l flex justify-evenly items-center pt-4'>
               <div className='justify-center flex items-center'>
               <Input placeholder='Enter your Search' />
-              <Button label='Search' className='mb-4' />
+              <Button label='Search' className='mb-4 ml-4' />
 
               </div>
+              <Button label=" New Post" className='rounded-lg mb-4 hover:bg-red-700 bg-red-400'/>
+            </div>
+            <div className='bg-white w-[60%]  mx-auto mt-32 p-8'>
+             <div className='border-b flex items-center  pb-4'>
+              {/* <Profile width={'50px'} height={'50px'} /> */}
+              <img src={Profile} height={'50px'} width={'50px'} alt="" />
+              <div className='ml-4'>
+
+              <h3>Lara Jane</h3>
+              <p>@Lara_Jane</p>
+              </div>
+             </div>
+             <div className='border-b pb-4 mb-4'>
+              <img className='h-[300px]' src={post} alt="" />
+             </div>
+             <div className='flex justify-evenly'>
+            <div>12.4k Likes</div>
+            <div>1.4k Comments</div>
+            <div>1k Shares</div>
+
+             </div>
+            </div>
+            <div className='bg-white w-[60%]  mx-auto mt-32 p-8'>
+             <div className='border-b flex items-center  pb-4'>
+              {/* <Profile width={'50px'} height={'50px'} /> */}
+              <img src={Profile} height={'50px'} width={'50px'} alt="" />
+              <div className='ml-4'>
+
+              <h3>Lara Jane</h3>
+              <p>@Lara_Jane</p>
+              </div>
+             </div>
+             <div className='border-b pb-4 mb-4'>
+              <img className='h-[300px]' src={post} alt="" />
+             </div>
+             <div className='flex justify-evenly'>
+            <div>12.4k Likes</div>
+            <div>1.4k Comments</div>
+            <div>1k Shares</div>
+
+             </div>
+            </div>
+
+            <div className='bg-white w-[60%]  mx-auto mt-32 p-8'>
+             <div className='border-b flex items-center  pb-4'>
+              {/* <Profile width={'50px'} height={'50px'} /> */}
+              <img src={Profile} height={'50px'} width={'50px'} alt="" />
+              <div className='ml-4'>
+
+              <h3>Lara Jane</h3>
+              <p>@Lara_Jane</p>
+              </div>
+             </div>
+             <div className='border-b pb-4 mb-4'>
+              <img className='h-[300px]' src={post} alt="" />
+             </div>
+             <div className='flex justify-evenly'>
+            <div>12.4k Likes</div>
+            <div>1.4k Comments</div>
+            <div>1k Shares</div>
+
+             </div>
+            </div>
+
+            <div className='bg-white w-[60%]  mx-auto mt-32 p-8'>
+             <div className='border-b flex items-center  pb-4'>
+              {/* <Profile width={'50px'} height={'50px'} /> */}
+              <img src={Profile} height={'50px'} width={'50px'} alt="" />
+              <div className='ml-4'>
+
+              <h3>Lara Jane</h3>
+              <p>@Lara_Jane</p>
+              </div>
+             </div>
+             <div className='border-b pb-4 mb-4'>
+              <img className='h-[300px]' src={post} alt="" />
+             </div>
+             <div className='flex justify-evenly'>
+            <div>12.4k Likes</div>
+            <div>1.4k Comments</div>
+            <div>1k Shares</div>
+
+             </div>
+            </div>
+
+            <div className='bg-white w-[60%]  mx-auto mt-32 p-8'>
+             <div className='border-b flex items-center  pb-4'>
+              {/* <Profile width={'50px'} height={'50px'} /> */}
+              <img src={Profile} height={'50px'} width={'50px'} alt="" />
+              <div className='ml-4'>
+
+              <h3>Lara Jane</h3>
+              <p>@Lara_Jane</p>
+              </div>
+             </div>
+             <div className='border-b pb-4 mb-4'>
+              <img className='h-[300px]' src={post} alt="" />
+             </div>
+             <div className='flex justify-evenly'>
+            <div>12.4k Likes</div>
+            <div>1.4k Comments</div>
+            <div>1k Shares</div>
+
+             </div>
+            </div>
+
+            <div className='bg-white w-[60%]  mx-auto mt-32 p-8'>
+             <div className='border-b flex items-center  pb-4'>
+              {/* <Profile width={'50px'} height={'50px'} /> */}
+              <img src={Profile} height={'50px'} width={'50px'} alt="" />
+              <div className='ml-4'>
+
+              <h3>Lara Jane</h3>
+              <p>@Lara_Jane</p>
+              </div>
+             </div>
+             <div className='border-b pb-4 mb-4'>
+              <img className='h-[300px]' src={post} alt="" />
+             </div>
+             <div className='flex justify-evenly'>
+            <div>12.4k Likes</div>
+            <div>1.4k Comments</div>
+            <div>1k Shares</div>
+
+             </div>
             </div>
         </div>
-        <div className='w-[20%] bg-slate-500'></div>
+        <div className='w-[20%] bg-[#F2F5F8]'></div>
 
     </div>
   )
